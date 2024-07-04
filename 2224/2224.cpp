042e7379b69f5d3e3efc89a79e2e2f214be027f2
Jean George <jean.george.alves@gmail.com>
2024-07-04 00:22:32 -0300

    finish 2224

--- 2224/2224.cpp
+++ 2224/2224.cpp
@@ -5,31 +5,70 @@
 
 using namespace std;
 
-class Point {
+class Participant {
    public:
     int x, y;
 };
 
-double calculateDistance(const Point &point1, const Point &point2) {
-    return sqrt(pow(point1.x - point2.x, 2) + pow(point1.y - point2.y, 2));
+const double MAX_POSSIBLE_DISTANCE = 10000.0;
+
+double calculateDistance(Participant &participant1, Participant &participant2) {
+    return sqrt(pow(participant1.x - participant2.x, 2) +
+                pow(participant1.y - participant2.y, 2));
 }
 
-double calculateMinimumDistance(const vector<Point> &points) { return 0; }
+int findFirstUnpaired(int mask, int numberOfParticipants) {
+    for (int i = 0; i < numberOfParticipants; i++) {
+        bool isUnpaired = !(mask & (1 << i));
+        if (isUnpaired) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+double calculateMinimumDistance(vector<Participant> &participants,
+                                int numberOfParticipants) {
+    int size = 1 << numberOfParticipants;
+    vector<double> dp(size, MAX_POSSIBLE_DISTANCE);
+    dp[0] = 0;
+
+    for (int mask = 0; mask < size; mask++) {
+        int firstUnpaired = findFirstUnpaired(mask, numberOfParticipants);
+
+        if (firstUnpaired == -1) {
+            continue;
+        }
+
+        for (int j = firstUnpaired + 1; j < numberOfParticipants; j++) {
+            bool isUnpaired = !(mask & (1 << j));
+            if (isUnpaired) {
+                int newMask = mask | 1 << firstUnpaired | 1 << j;
+                double pairCost = calculateDistance(participants[firstUnpaired],
+                                                    participants[j]);
+                dp[newMask] = min(dp[newMask], dp[mask] + pairCost);
+            }
+        }
+    }
+
+    return dp[size - 1];
+}
 
 int main() {
-    int instances, participants;
+    int instances, numberOfParticipants;
 
     cin >> instances;
 
     for (int i = 0; i < instances; i++) {
-        cin >> participants;
-        vector<Point> points(participants);
+        cin >> numberOfParticipants;
+        vector<Participant> participants(numberOfParticipants);
 
-        for (int j = 0; j < participants; j++) {
-            cin >> points[j].x >> points[j].y;
+        for (int j = 0; j < numberOfParticipants; j++) {
+            cin >> participants[j].x >> participants[j].y;
         }
 
-        cout << fixed << setprecision(2) << calculateMinimumDistance(points)
+        cout << fixed << setprecision(2)
+             << calculateMinimumDistance(participants, numberOfParticipants)
              << endl;
     }
 
