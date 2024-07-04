d19c8e57673d6f8d4285be0aea59cf1fe4b0325e
Jean George <jean.george.alves@gmail.com>
2024-07-04 00:51:07 -0300

    add 1054 structure

--- 1054/1054.cpp
+++ 1054/1054.cpp
@@ -1,9 +1,35 @@
 
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class Stone {
+   public:
+    char size;
+    int position;
+};
+
+int calculateMaxDistance() { return 1; }
+
 int main() {
-    //
+    int numberOfTestCases, numberOfStones, distance;
+
+    cin >> numberOfTestCases;
+
+    for (int testCase = 1; testCase <= numberOfTestCases; testCase++) {
+        cin >> numberOfStones >> distance;
+        vector<Stone> stones(numberOfStones);
+
+        for (int i = 0; i < numberOfStones; i++) {
+            cin >> stones[i].size;
+            cin.ignore(1, '-');
+            cin >> stones[i].position;
+        }
+
+        cout << "Case " << testCase << ": " << calculateMaxDistance();
+    }
+
     return 0;
 }
